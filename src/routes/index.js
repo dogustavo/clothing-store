ee0edae28f5338edcf6701fa844af37512411d66
gustavo <gustavo.oliveira@atuo.in>
2020-05-01 11:41:17 -0300

    adicionado componente header

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,9 +7,11 @@ import {
 
 import Home from '../pages/homepage';
 import ShopPage from '../pages/shopPage/shop';
+import Header from '../components/header';
 
 const Routes = () => (
     <BrowserRouter>
+        <Header/>
         <Switch>
             <Route exact path='/' component={Home}/>
             <Route path='/shop' component={ShopPage}/>
