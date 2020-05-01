29fe7ad483445478acb22e3acd540c8e43bd5415
gustavo <gustavo.oliveira@atuo.in>
2020-05-01 10:25:59 -0300

    atualizado menu-items para navegar com withRouter HOC

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,20 @@ import {
     Route
 } from 'react-router-dom';
 
-import Home from '../pages/homepage'
+import Home from '../pages/homepage';
+
+
+const HatsPage = () => (
+    <>
+        <h1>Hats Page</h1>
+    </>
+)
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path='/' component={Home}/>
+            <Route path='/hats' component={HatsPage}/>
         </Switch>
     </BrowserRouter>
 );
