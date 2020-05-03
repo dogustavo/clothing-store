737b57f4eaefb496cb157220067225306ca748a6
gustavo <gustavo.oliveira@atuo.in>
2020-05-03 10:21:43 -0300

    adicionado função de salvar user no banco

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -13,6 +13,34 @@ const firebaseConfig = {
     measurementId: "G-H6GYNBW632"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) {
+        return;
+    }
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const created_at = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                created_at,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
